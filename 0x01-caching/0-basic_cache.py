686da0f223bcb63c81167500a69bc374e32bd20e
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-30 20:35:16 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" BasicCache module """
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """Assign the item to the cache with the specified key."""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve the item associated with the given key."""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
